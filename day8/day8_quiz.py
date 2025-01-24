054cd486b646716519fa07326d7787773a319f20
cracker-y <cracker0dl@gmail.com>
2025-01-25 00:27:43 +0900

    ✅ MongoDB 문제 풀어보기
    미완성입니다 .. 쉬는날 꼭 전부 풀어서 다시 올리겠습니다!!

---
+++ day8/day8_quiz.py
@@ -0,0 +1,17 @@
+from day8.MongoDBQuizDay8 import client
+
+db = client['day8_quiz_db']
+books = db["books"]
+movies = db["movies"]
+user_actions = db["suer_actions"]
+
+
+books_col = books.find()
+
+# 문제 1
+for book in books.find():
+    print(books_col.collection)
+
+
+# 쉬는날 전부다 풀어서 다시 올리겠습니다!!!!!!
+
